edcbd4e80c6d3546f7ded89e877b77440e2160fa
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2026-02-23 16:45:05 +0530

    done user schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,14 @@
 import express from "express"
 import dotenv  from "dotenv"
 import mongoose from "mongoose"
+import cookieParser from "cookie-parser"
 import { MONGO_URL } from "./config.js"
 dotenv.config()
 
 const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
 
 
 
