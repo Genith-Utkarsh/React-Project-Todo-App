f97c85f9d05036a543e02961bbb3b2b4f5526e14
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2026-02-25 15:49:39 +0530

    done signin endpoint

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,46 +1,80 @@
-import type { Request, Response } from "express";
-import { User } from "../db/user.js";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken"
-import { JWT_SECRET } from "../config.js";
+import type { Request, Response } from 'express';
+import { User } from '../db/user.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../config.js';
 
-
-
-
-export const userSignUp = async(req : Request , res : Response) =>{
+export const userSignUp = async (req: Request, res: Response) => {
   try {
-    const { email, password } = req.body
+    const { email, password } = req.body;
 
-    // if user already exist 
-    const isUserExist = await User.findOne({email})
+    // if user already exist
+    const isUserExist = await User.findOne({ email });
 
-    if(isUserExist)
-    {
+    if (isUserExist) {
       return res.status(400).json({
-        msg : "User already exist in our database"
-      })
+        msg: 'User already exist in our database',
+      });
     }
 
     const hashedPassword = await bcrypt.hash(password, 5);
 
     const user = await User.create({
-      email : email ,
-      password : hashedPassword
-    })
+      email: email,
+      password: hashedPassword,
+    });
 
-    const token  = jwt.sign({userId : user._id}, JWT_SECRET);
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET);
 
-    res.cookie("token", token);
+    res.cookie('token', token);
 
-    res.json({
-      msg : "Sign Up successfull",
+    res.status(200).json({
+      msg: 'Sign Up successfull',
+      token: token,
+    });
+  } catch (err) {
+    res.status(500).json({
+      msg: 'server issue : unable to create a user',
+    });
+  }
+};
+
+export const userSignIn = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const isUserExist = await User.findOne({email});
+
+    if(!isUserExist)
+    {
+      return res.status(400).json({
+        msg : "User doesnot exist in db , please sign up first"
+      })
+    }
+
+    const comparePass = await bcrypt.compare(password, isUserExist.password as string)
+
+    if(!comparePass)
+    {
+      return res.status(400).json({
+        msg : "Wrong password"
+      })
+    }
+
+
+    const token  = jwt.sign({userId : isUserExist._id}, JWT_SECRET)
+
+    res.cookie("token", token)
+
+    res.status(200).json({
+      msg : "Signin Successful.",
       token : token
     })
+
     
-  } catch(err)
-  {
+  } catch (err) {
     res.status(500).json({
-      msg : "server issue : unable to create a user"
-    })
+      msg: 'Server issue : Unable to sign in',
+    });
   }
-}
+};
